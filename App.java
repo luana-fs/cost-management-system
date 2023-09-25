e7d1ae42f5f13d9dc87aa821ba8efa4748072d58
raulyug <120514382+raulyug@users.noreply.github.com>
2023-09-25 18:50:30 -0300

    Removed App Constructor, added main, placed arrays and variables declarations inside main.

--- App.java
+++ App.java
@@ -3,13 +3,40 @@ import java.util.Locale;
 
 public class App {
 
-	private static Funcionario funcionarioLogado;
-	private Scanner entrada;
+    private static Funcionario funcionarioLogado;
+    private Scanner entrada;
 
-    public App() {
-        entrada = new Scanner(System.in);
+    public static void main(String args[]) {
+
+        Departamento[] departamentos = new Departamento[6];
+        Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.ENGLISH);
-        enxame = new Enxame();
+        
+        // Estanciando os departamentos
+        Departamento rh = new Departamento("RH");
+        Departamento compras = new Departamento("Compras");
+        Departamento vendas = new Departamento("Vendas");
+        Departamento expedicao = new Departamento("Expedição");
+        Departamento engenharia = new Departamento("Engenharia");
+        Departamento producao = new Departamento("Produção");
+
+        // Populando o array
+        departamentos[0] = rh;
+        departamentos[1] = compras;
+        departamentos[2] = vendas;
+        departamentos[3] = expedicao;
+        departamentos[4] = engenharia;
+        departamentos[5] = producao;
+
+    }
+
+    private void menu() {
+        System.out.println("=====================================");
+        System.out.println("Menu de opcoes: ");
+        System.out.println("[0] Sair do sistema");
+        System.out.println("[1] Acessar");
+        System.out.println("[2] Cadastrar novo funcionario");
+        System.out.println("=====================================");
     }
 
     public void executar() {
@@ -31,41 +58,6 @@ public class App {
                 default:
                     System.out.println("Selecione uma opção valida!");
             }
-        } while (opcao !=0);
+        } while (opcao != 0);
     }
-    private void menu() {
-        System.out.println("=====================================");
-        System.out.println("Menu de opcoes: ");
-        System.out.println("[0] Sair do sistema");
-        System.out.println("[1] Acessar");
-        System.out.println("[2] Cadastrar novo funcionario");
-        System.out.println("=====================================");
-    }
-
-	public static void main(String args[])
-	{
-		Departamento[] departamentos = new Departamento[6];
-
-	//Estanciando os departamentos
-		Departamento rh = new Departamento("RH");
-		Departamento compras = new Departamento("Compras");
-		Departamento vendas = new Departamento("Vendas");
-		Departamento expedicao = new Departamento("Expedição");
-		Departamento engenharia = new Departamento("Engenharia");
-		Departamento producao = new Departamento("Produção");
-
-	//Populando o array
-		departamentos[0] = rh;
-		departamentos[1] = compras;
-		departamentos[2] = vendas;
-		departamentos[3] = expedicao;
-		departamentos[4] = engenharia ;
-		departamentos[5] = producao;
-		
-
-
-
-
-
-	}
 }
