0b663cc35875a2ae25c1965a9b615c1c8fe555d4
leoramosjr <leonardo-ramos@uergs.edu.br>
2023-09-28 22:15:35 -0300

    refact: refacted menu. added menu before user is logged

--- App.java
+++ App.java
@@ -67,34 +67,104 @@ public class App {
 
     }
 
+    private static boolean startupMenu() {
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 0;
+        System.out.println("=====================================");
+        System.out.print("Olá! Seja bem vindo ao sistema de controle de custos da empresa!");
+        System.out.println("Selecione a forma de acesso:");
+        while(opcao != 1 && opcao != 2) {
+            System.out.println("[1] Funcionário");
+            System.out.println("[2] Administrador");
+            System.out.println("=====================================");
+            opcao = entrada.nextInt();  
+            entrada.nextLine();
+            switch (opcao) {
+                case 1:
+                    // AcessarComoFuncionario();
+                    break;
+                case 2:
+                    // CadastrarNovoFuncionario();
+                    break;
+                default: System.out.println("Selecione uma opção valida!");
+            }
+        }
+        return true;
+    }
+
     private static void menu() {
         System.out.println("=====================================");
         System.out.println("Menu de opcoes: ");
+        System.out.println("[1] Registrar novo custo");
+        System.out.println("[2] Mostrar estatísticas");
+        System.out.println("[3] Pesquisar custo");
+        System.out.println("[4] Visualizar dados pessoais");
+        System.out.println("[5] Visualizar custos do mês");
+        System.out.println("[6] Vizualizar custos dos últimos 3 meses");
+        System.out.println("[7] Top 3 funcionários com maiores registros");
         System.out.println("[0] Sair do sistema");
-        System.out.println("[1] Acessar");
-        System.out.println("[2] Cadastrar novo funcionario");
-        System.out.println("[3] Pesquisar registros de custo");
         System.out.println("=====================================");
     }
 
     public static void executar() {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.ENGLISH);
-
+        boolean logado = false;
         int opcao = 0;
+
+        while (logado == false) {
+            logado = startupMenu();
+        }
+
         do {
             menu();
             System.out.print("Insira a opção desejada:");
             opcao = entrada.nextInt();
             entrada.nextLine();
             switch (opcao) {
-                case 0:
+                case 0: opcao = 0;
                     break;
                 case 1:
                     // AcessarComoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
                     break;
                 case 2:
                     // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
+                    break;
+                case 3: 
+                    // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
+                    break;
+                case 4:
+                    // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
+                    break;
+                case 5:
+                    // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
+                    break;
+                case 6:
+                    // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
+                    break;
+                case 7:
+                    // CadastrarNovoFuncionario();
+                    menu();
+                    opcao = entrada.nextInt();
+                    entrada.nextLine();
                     break;
                 default:
                     System.out.println("Selecione uma opção valida!");
