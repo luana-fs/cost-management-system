07adc803521fe2270434558845ce6edf4f9a38df
SaletteCosta <133251126+SaletteCosta@users.noreply.github.com>
2023-09-29 10:44:50 -0300

    add menuPesquisa

--- App.java
+++ App.java
@@ -4,10 +4,12 @@ import java.util.Locale;
 public class App {
 
   private static Funcionario funcionarioLogado;
+  private static RegistroDeCusto registroDeCusto;
 
   public static void main(String[] args) {
 
   Departamento[] departamentos = new Departamento[6];
+  registroDeCusto = new RegistroDeCusto(); 
 
   //Estanciando os departamentos
   Departamento rh = new Departamento("RH");
@@ -96,9 +98,77 @@ public class App {
                 case 2:
                     // CadastrarNovoFuncionario();
                     break;
+                case 3:
+                    menuPesquisa();
+                    int opcaoPesquisa = entrada.nextLine();
+                    executarPesquisa(opcaoPesquisa);
+                    break;
                 default:
                     System.out.println("Selecione uma opção valida!");
             }
         } while (opcao != 0);
     }
+    private static void menuPesquisa() {
+        System.out.println("=====================================");
+        System.out.println("Menu de Pesquisa: ");
+        System.out.println("[1] Pesquisar por Descrição");
+        System.out.println("[2] Pesquisar por Categoria");
+        System.out.println("[3] Pesquisar por Data");
+        System.out.println("[4] Pesquisar por Departamento");
+        System.out.println("[0] Voltar ao menu principal");
+        System.out.println("=====================================");
+    }
+
+    private static void executarPesquisa(String opcao) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.ENGLISH);
+
+        switch (opcao) {
+            case "0":
+                break;
+            case "1":
+                System.out.print("Digite a descrição que deseja pesquisar: ");
+                String descricao = entrada.nextLine();
+                RegistroDeCusto resultadoDescricao = registroDeCusto.pesquisaDescricao(descricao);
+                if (resultadoDescricao != null) {
+                    System.out.println("Resultado da pesquisa:\n" + resultadoDescricao.toString());
+                } else {
+                    System.out.println("Nenhum resultado encontrado.");
+                }
+                break;
+            case "2":
+                System.out.print("Digite a categoria que deseja pesquisar: ");
+                String categoria = entrada.nextLine();
+                RegistroDeCusto resultadoCategoria = registroDeCusto.pesquisarCategoria(categoria);
+                if (resultadoCategoria != null) {
+                    System.out.println("Resultado da pesquisa:\n" + resultadoCategoria.toString());
+                } else {
+                    System.out.println("Nenhum resultado encontrado.");
+                }
+                break;
+            case "3":
+                System.out.print("Digite a data que deseja pesquisar: ");
+                String data = entrada.nextLine();
+                RegistroDeCusto resultadoData = registroDeCusto.pesquisarData(data);
+                if (resultadoData != null) {
+                    System.out.println("Resultado da pesquisa:\n" + resultadoData.toString());
+                } else {
+                    System.out.println("Nenhum resultado encontrado.");
+                }
+                break;
+            case "4":
+                System.out.print("Digite o departamento o departamento: ");
+                String departamento = entrada.nextLine();
+                RegistroDeCusto resultadoDepartamento = registroDeCusto.pesquisarDepartamento(departamento);
+                if (resultadoDepartamento != null) {
+                    System.out.println("Resultado da pesquisa:\n" + resultadoDepartamento.toString());
+                } else {
+                    System.out.println("Nenhum resultado encontrado.");
+                }
+                break;
+            default:
+                System.out.println("Selecione uma opção válida.");
+        }
+    }
+
 }
--- RegistroDeCusto.java
+++ RegistroDeCusto.java
@@ -95,6 +95,5 @@ public class RegistroDeCusto {
     return null;
 	  }
      
-  
 }
   
