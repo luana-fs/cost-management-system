21f6511c5f19aead6aa8168597e08f88c5a3b172
Pedro Henrique <111026471+PedroRamos02@users.noreply.github.com>
2023-09-28 21:35:53 -0300

    implementação da classe Cadastro

---
+++ Cadastro.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Cadastro {
+
+    private ArrayList<Funcionario> funcionarios;
+
+    public Cadastro() {funcionarios = new ArrayList<Funcionario>(10);}
+
+    public boolean cadastraFuncionario(Funcionario f) { return robos.add(f);}
+
+    
+}
