ed2129727f56e82b14e072d9a396b2a8025afec1
Fernanda Farias <143156498+FeFeFarias05@users.noreply.github.com>
2023-09-29 10:21:22 -0300

    Correção de um erro de implementação

--- App.java
+++ App.java
@@ -17,13 +17,6 @@ public class App {
   Departamento engenharia = new Departamento("Engenharia");
   Departamento producao = new Departamento("Produção");
 
-  // Populando o array
-  departamentos[0] = rh;
-  departamentos[1] = compras;
-  departamentos[2] = vendas;
-  departamentos[3] = expedicao;
-  departamentos[4] = engenharia;
-  departamentos[5] = producao;
 
 	//Populando o array
 	System.out.println("O departamento foi cadastrdo com sucesso? " + App.insereDep(departamentos, rh));
